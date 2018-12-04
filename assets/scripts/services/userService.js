453d0150aaf4c5a7fc16792e07ed9613e6b1a367
Calvin <31435496+Calvin1121@users.noreply.github.com>
2018-12-04 18:02:09 +0800

    remove jauery

--- assets/scripts/services/userService.js
+++ assets/scripts/services/userService.js
@@ -1,41 +1,43 @@
 const urlConstant = require('urlConstant');
+const request = new XMLHttpRequest();
 module.exports = {
-    login(username, password){
-        return new Promise((resolve, reject)=>{
+    login(username, password) {
+        return new Promise((resolve, reject) => {
+            let url = urlConstant.BASE_URL + urlConstant.LOGIN;
             let formData = new FormData();
             formData.append("username", username);
             formData.append("password", password);
             formData.append("grant_type", "password");
             formData.append("client_id", "1QOCTIlUrESYWpDNf9keR6OwAehoAzd5ISgwvglh");
-            formData.append("client_secret", "jQOb5WCV2betjVZYuDlqSYKrvmwF03dTCrE4NwQYXF8bHmzpkLCWfS8lxefbBE6zVOOOQbS9grHBCShkPX3oXzLgWdXnDA59kOmi1BcFpiDtOLOx3guQ7Ry966R5nv9q"); 
-            $.post({
-                url:urlConstant.BASE_URL + urlConstant.LOGIN,
-                data:formData,
-                processData:false,
-                contentType:false,
-                success:(data)=>{
-                    resolve(data);
-                },
-                error:(error)=>{   
-                    reject(error.responseJSON);
+            formData.append("client_secret", "jQOb5WCV2betjVZYuDlqSYKrvmwF03dTCrE4NwQYXF8bHmzpkLCWfS8lxefbBE6zVOOOQbS9grHBCShkPX3oXzLgWdXnDA59kOmi1BcFpiDtOLOx3guQ7Ry966R5nv9q");
+            request.open('POST', url, true);
+            request.send(formData);
+            request.onreadystatechange = ()=> {
+                if(request.readyState === 4){
+                    if(request.status >= 200 && request.status < 400){
+                        resolve(JSON.parse(request.response));
+                    }else{
+                        reject(JSON.parse(request.response));
+                    }
                 }
-            })
+            };
         })
     },
-    getCurrentUser(token){
-        return new Promise((resolve, reject)=>{
-            $.get({
-                url:urlConstant.BASE_URL + urlConstant.CURRENT_USER,
-                headers: {
-                    Authorization: 'Bearer ' + token,
-                },
-                success:(data)=>{
-                    resolve(data);
-                },
-                error:(error)=>{   
-                    reject(error.responseJSON);
+    getCurrentUser(token) {
+        return new Promise((resolve, reject) => {
+            let url = urlConstant.BASE_URL + urlConstant.CURRENT_USER;
+            request.open('GET', url, true);
+            request.setRequestHeader('Authorization',`Bearer ${token}`);
+            request.send();
+            request.onreadystatechange = ()=> {
+                if(request.readyState === 4){
+                    if(request.status >= 200 && request.status < 400){
+                        resolve(JSON.parse(request.response));
+                    }else{
+                        reject(JSON.parse(request.response));
+                    }
                 }
-            })
+            };
         })
     },
 };
